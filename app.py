75c04187396e1ff9f51dc297b4a2fb26baccd670
Sahil Chadha <Chadhas282@gmail.com>
2026-09-18 08:54:01 +0530

    Refactor app.py to include additional imports for image processing

--- app.py
+++ app.py
@@ -3,3 +3,7 @@ from fastapi.responses import HTMLResponse
 from fastapi.staticfiles import StaticFiles
 import tensorflow as tf
 import numpy as np
+from PIL import Image
+import io
+
+app = FastAPI()
